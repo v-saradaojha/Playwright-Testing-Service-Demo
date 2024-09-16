7ca860abb64a99b65988168f36cc3203d469d946
Sarada Ojha <v-saradaojha@microsoft.com>
2024-09-16 23:33:17 +0530

    initial repo added

---
+++ tests/example.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+// This sample simulates a larger test suite
+const tags = ['@smoke', '@sanity', '@issue'];
+//eastasia test//
+const TEST_ITERATIONS = parseInt(process.env.TEST_ITERATIONS || "1");
+for (let i = 0; i < TEST_ITERATIONS; i++) {
+  let tagid = i % 3;
+  test('has title ' + i + tags[tagid], async ({ page }) => {
+    const skip = Math.random();
+    if (skip > 0.9) {
+      test.skip();
+    }
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
+  test('get started link ' + i + tags[tagid], async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+
+    // Expects the URL to contain intro.
+    await expect(page).toHaveURL(/.*intro/);
+
+    const randomResult = Math.random();
+    expect(randomResult).toBeGreaterThan(0.2);
+  });
+}
