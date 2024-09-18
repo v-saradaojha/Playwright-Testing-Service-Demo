340ab29b0871482ec21fa15e13583f1a034f7988
Sarada Ojha <v-saradaojha@microsoft.com>
2024-09-18 13:29:05 +0530

    test run

---
+++ Samples/GetStarted/playwright.service.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from '@playwright/test';
+import { getServiceConfig, ServiceOS } from '@azure/microsoft-playwright-testing';
+import config from './playwright.config';
+
+/* Learn more about service configuration at https://aka.ms/mpt/config */
+
+export default defineConfig(
+  config,
+  getServiceConfig(config, {
+    serviceAuthType: 'ACCESS_TOKEN',  
+    useCloudHostedBrowsers: true,
+    timeout: 30000,
+    os: ServiceOS.LINUX,
+  }),
+  {
+    /* 
+    Playwright Testing service reporter is added by default.
+    This will override any reporter options specified in the base playwright config.
+    If you are using more reporters, please update your configuration accordingly.
+    */
+    reporter: [['list'], ['@azure/microsoft-playwright-testing/reporter']],
+  }
+);
