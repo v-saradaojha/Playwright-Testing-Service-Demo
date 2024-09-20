82535efb8634c666442f560deae2bf67dc120ae5
Sarada Ojha <v-saradaojha@microsoft.com>
2024-09-20 16:02:55 +0530

    All on test 20902024

--- Samples/GetStarted/playwright.config.ts
+++ Samples/GetStarted/playwright.config.ts
@@ -28,7 +28,9 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    video:'on',
+    screenshot :'on'
   },
 
   /* Configure projects for major browsers */
