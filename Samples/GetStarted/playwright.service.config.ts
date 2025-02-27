bf7c2f28c30f59a5167e48229f6fb62e7e10129b
Sarada Ojha <v-saradaojha@microsoft.com>
2025-02-27 13:04:46 +0530

    validate

--- Samples/GetStarted/playwright.service.config.ts
+++ Samples/GetStarted/playwright.service.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(
   getServiceConfig(config, {
     runId: process.env.PLAYWRIGHT_SERVICE_RUN_ID,
     runName: "Testing Service Demo" + new Date().toLocaleString(),
-    serviceAuthType: 'ENTRA_ID',
+    // serviceAuthType: 'ACCESS_TOKEN',    
     useCloudHostedBrowsers: true,
     timeout: 30000,
     os: ServiceOS.LINUX,
